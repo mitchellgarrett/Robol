21f4755ac1a32fc7c20eab82b73c50d62f18121c
Mitchell Garrett <45768600+mitchellgarrett@users.noreply.github.com>
2024-03-04 20:09:40 -0700

    Add integer arithmetic tests

--- src/vm/VirtualMachine.cs
+++ src/vm/VirtualMachine.cs
@@ -2,20 +2,17 @@
 using System.Collections.Generic;
 using FTG.Studios.Robol.Compiler;
 
-namespace FTG.Studios.Robol.VM
-{
+namespace FTG.Studios.Robol.VM {
 
 	public enum MessageSeverity { Normal, Warning, Error };
 
-	public class VirtualMachine
-	{
+	public class VirtualMachine {
 
 		readonly ParseTree program;
 		readonly SymbolTable globalScope;
 		SymbolTable localScope;
 
-		public VirtualMachine(ParseTree program)
-		{
+		public VirtualMachine(ParseTree program) {
 			this.program = program;
 			globalScope = new SymbolTable();
 		}
@@ -24,78 +21,66 @@ namespace FTG.Studios.Robol.VM
 		public void RegisterConsoleOutput(Action<object, MessageSeverity> cb) { ConsoleOutput += cb; }
 		public void UnregisterConsoleOutput(Action<object, MessageSeverity> cb) { ConsoleOutput -= cb; }
 
-		public object Run()
-		{
+		public object Run() {
 			globalScope.Clear();
 			Library.AddBuiltinFunctionsToSymbolTable(globalScope);
 
 			// Define functions
 			ParseTree.FunctionList list = program.Root.List;
-			while (list != null)
-			{
+			while (list != null) {
 				if (!globalScope.IsDeclared(list.Function.Identifier.Value)) globalScope.DefineSymbol(list.Function.Identifier.Value, typeof(void), list.Function);
 				list = list.List;
 			}
 
-			try
-			{
+			try {
 				return EvaluateProgram(program.Root);
-			}
-			catch (Exception exception)
-			{
+			} catch (Exception exception) {
 				ConsoleOutput?.Invoke($"{exception.GetType()}:\n{exception.Message}", MessageSeverity.Error);
 			}
 			return null;
 		}
 
 		#region Program
-		object EvaluateProgram(ParseTree.Program program)
-		{
+		object EvaluateProgram(ParseTree.Program program) {
 			localScope = globalScope.PushScope();
 			object result = EvaluateFunction(program.Main);
 			localScope = localScope.PopScope();
 			return result;
 		}
 
-		object EvaluateFunction(ParseTree.Function function)
-		{
+		object EvaluateFunction(ParseTree.Function function) {
 			if (function is ParseTree.BuiltinFunction) return Library.EvaluateBuiltinFunction(function as ParseTree.BuiltinFunction, localScope);
 			return EvaluateStatementList(function.Body);
 		}
 		#endregion
 
 		#region Statements
-		object EvaluateStatementList(ParseTree.StatementList list)
-		{
+		object EvaluateStatementList(ParseTree.StatementList list) {
 			if (list == null) return null;
 			if (list.Statement is ParseTree.Return) return EvaluateStatement(list.Statement as ParseTree.Return);
 			EvaluateStatement(list.Statement);
 			return EvaluateStatementList(list.List);
 		}
 
-		object EvaluateStatement(ParseTree.Statement statement)
-		{
+		object EvaluateStatement(ParseTree.Statement statement) {
 			if (statement is ParseTree.Return) return EvaluateStatement(statement as ParseTree.Return);
 			if (statement is ParseTree.Declaration) return EvaluateStatement(statement as ParseTree.Declaration);
 			if (statement is ParseTree.Assignment) return EvaluateStatement(statement as ParseTree.Assignment);
 			return null;
 		}
 
-		object EvaluateStatement(ParseTree.Return statement)
-		{
+		object EvaluateStatement(ParseTree.Return statement) {
 			return EvaluateExpression(statement.Expression);
 		}
 
-		object EvaluateStatement(ParseTree.Declaration statement)
-		{
+		object EvaluateStatement(ParseTree.Declaration statement) {
 			if (!localScope.IsDeclared(statement.Identifier.Value)) localScope.DeclareSymbol(statement.Identifier.Value, statement.Type);
 			Symbol symbol = localScope.GetSymbol(statement.Identifier.Value);
 			symbol.SetValue(EvaluateExpression(statement.Expression));
 			return null;
 		}
 
-		object EvaluateStatement(ParseTree.Assignment statement)
-		{
+		object EvaluateStatement(ParseTree.Assignment statement) {
 			Symbol symbol = localScope.GetSymbol(statement.Identifier.Value);
 
 			if (symbol == null) throw new ArgumentNullException($"({statement.Line}, {statement.Column}): '{statement.Identifier}'", $"Variable not defined");
@@ -106,18 +91,15 @@ namespace FTG.Studios.Robol.VM
 		#endregion
 
 		#region Expressions
-		object EvaluateExpression(ParseTree.Expression expression)
-		{
+		object EvaluateExpression(ParseTree.Expression expression) {
 			if (expression is ParseTree.UnaryExpression) return EvaluateExpression(expression as ParseTree.UnaryExpression);
 			if (expression is ParseTree.ArithmeticExpression) return EvaluateExpression(expression as ParseTree.ArithmeticExpression);
 			if (expression is ParseTree.LogicalExpression) return EvaluateExpression(expression as ParseTree.LogicalExpression);
 			return null;
 		}
 
-		float EvaluateExpression(ParseTree.UnaryExpression expression)
-		{
-			switch (expression.Operator)
-			{
+		float EvaluateExpression(ParseTree.UnaryExpression expression) {
+			switch (expression.Operator) {
 				case Syntax.operator_subtraction:
 					return expression.Primary.GetType() == typeof(ParseTree.IntegerConstant)
 						? -EvaluatePrimary(expression.Primary as ParseTree.IntegerConstant)
@@ -127,45 +109,38 @@ namespace FTG.Studios.Robol.VM
 		}
 
 		// TODO: Make these work
-		bool EvaluateExpression(ParseTree.LogicalExpression expression)
-		{
+		bool EvaluateExpression(ParseTree.LogicalExpression expression) {
 			return EvaluateExpression(expression.Expression);
 		}
 
-		bool EvaluateExpression(ParseTree.LogicalOrExpression expression)
-		{
+		bool EvaluateExpression(ParseTree.LogicalOrExpression expression) {
 			bool rhs = EvaluateExpression(expression.RightExpression);
 			if (expression.LeftExpression != null) rhs = EvaluateExpression(expression.LeftExpression) || rhs;
 			return rhs;
 		}
 
-		bool EvaluateExpression(ParseTree.LogicalAndExpression expression)
-		{
+		bool EvaluateExpression(ParseTree.LogicalAndExpression expression) {
 			return EvaluateExpression(expression.LeftExpression) && EvaluateExpression(expression.RightExpression);
 		}
 
-		float EvaluateExpression(ParseTree.ArithmeticExpression expression)
-		{
+		float EvaluateExpression(ParseTree.ArithmeticExpression expression) {
 			float lhs = (float)EvaluateExpression(expression.LeftExpression);
 			if (expression.Operator == '\0') return lhs;
 			float rhs = (float)EvaluateExpression(expression.RightExpression);
 
-			switch (expression.Operator)
-			{
+			switch (expression.Operator) {
 				case Syntax.operator_addition: return lhs + rhs;
 				case Syntax.operator_subtraction: return lhs - rhs;
 			}
 			return 0;
 		}
 
-		float EvaluateExpression(ParseTree.MultiplicativeExpression expression)
-		{
+		float EvaluateExpression(ParseTree.MultiplicativeExpression expression) {
 			float lhs = (float)EvaluateExpression(expression.LeftExpression);
 			if (expression.Operator == '\0') return lhs;
 			float rhs = (float)EvaluateExpression(expression.RightExpression);
 
-			switch (expression.Operator)
-			{
+			switch (expression.Operator) {
 				case Syntax.operator_multiplication: return lhs * rhs;
 				case Syntax.operator_division: return lhs / rhs;
 				case Syntax.operator_modulus: return lhs % rhs;
@@ -173,8 +148,7 @@ namespace FTG.Studios.Robol.VM
 			return 0;
 		}
 
-		float EvaluateExpression(ParseTree.ExponentialExpression expression)
-		{
+		float EvaluateExpression(ParseTree.ExponentialExpression expression) {
 			float lhs =
 				expression.LeftExpression.GetType() == typeof(ParseTree.IntegerConstant)
 				? (float)(int)EvaluatePrimary(expression.LeftExpression)
@@ -192,8 +166,7 @@ namespace FTG.Studios.Robol.VM
 		#endregion
 
 		#region Primaries
-		object EvaluatePrimary(ParseTree.Primary primary)
-		{
+		object EvaluatePrimary(ParseTree.Primary primary) {
 			if (primary is ParseTree.Identifier) return EvaluatePrimary(primary as ParseTree.Identifier);
 			if (primary is ParseTree.FunctionCall) return EvaluatePrimary(primary as ParseTree.FunctionCall);
 			if (primary is ParseTree.IntegerConstant) return EvaluatePrimary(primary as ParseTree.IntegerConstant);
@@ -204,8 +177,7 @@ namespace FTG.Studios.Robol.VM
 			return null;
 		}
 
-		object EvaluatePrimary(ParseTree.Identifier primary)
-		{
+		object EvaluatePrimary(ParseTree.Identifier primary) {
 			Symbol symbol = localScope.GetSymbol(primary.Value);
 
 			if (symbol == null) throw new ArgumentNullException($"Variable '{primary.Value}' does not exist!");
@@ -213,40 +185,33 @@ namespace FTG.Studios.Robol.VM
 			return symbol.Value;
 		}
 
-		object EvaluatePrimary(ParseTree.FunctionCall primary)
-		{
+		object EvaluatePrimary(ParseTree.FunctionCall primary) {
 			return EvaluateFunctionCall(primary);
 		}
 
-		int EvaluatePrimary(ParseTree.IntegerConstant primary)
-		{
+		int EvaluatePrimary(ParseTree.IntegerConstant primary) {
 			return primary.Value;
 		}
 
-		float EvaluatePrimary(ParseTree.NumberConstant primary)
-		{
+		float EvaluatePrimary(ParseTree.NumberConstant primary) {
 			return primary.Value;
 		}
 
-		string EvaluatePrimary(ParseTree.StringConstant primary)
-		{
+		string EvaluatePrimary(ParseTree.StringConstant primary) {
 			return primary.Value;
 		}
 
-		bool EvaluatePrimary(ParseTree.BooleanConstant primary)
-		{
+		bool EvaluatePrimary(ParseTree.BooleanConstant primary) {
 			return primary.Value;
 		}
 
-		object EvaluateFunctionCall(ParseTree.FunctionCall call)
-		{
+		object EvaluateFunctionCall(ParseTree.FunctionCall call) {
 			ParseTree.Function function = localScope.GetSymbol(call.Identifier.Value).Value as ParseTree.Function;
 
 			ParseTree.ParameterList plist = function.Parameters;
 			ParseTree.ArgumentList alist = call.Arguments;
 			List<(string, Type, object)> parameters = new List<(string, Type, object)>();
-			while (plist != null && alist != null)
-			{
+			while (plist != null && alist != null) {
 				ParseTree.Parameter param = plist.Parameter;
 				ParseTree.Argument arg = alist.Argument;
 
@@ -257,8 +222,7 @@ namespace FTG.Studios.Robol.VM
 			}
 
 			localScope = localScope.PushAdjacentScope();
-			foreach ((string identifier, Type type, object value) in parameters)
-			{
+			foreach ((string identifier, Type type, object value) in parameters) {
 				localScope.DefineSymbol(identifier, type, value);
 			}
 
--- test/VirtualMachineTest.cs
+++ test/VirtualMachineTest.cs
@@ -20,11 +20,16 @@ namespace FTG.Studios.Robol.Test {
         [Test]
         public void ReturnStatement_ReturnsIntegerConstant() {
             const int expected = 42;
+
+
             ParseTree program = Compiler.Compiler.Compile($"int main() {{ return {expected}; }}");
+
+
             VirtualMachine vm = new VirtualMachine(program);
 
             object actual = vm.Run();
 
+            Assert.AreEqual(typeof(int), actual.GetType());
             Assert.AreEqual(expected, actual);
         }
 
@@ -36,6 +41,7 @@ namespace FTG.Studios.Robol.Test {
 
             object actual = vm.Run();
 
+            Assert.AreEqual(typeof(float), actual.GetType());
             Assert.AreEqual(expected, actual);
         }
 
@@ -47,7 +53,141 @@ namespace FTG.Studios.Robol.Test {
 
             object actual = vm.Run();
 
+            Assert.AreEqual(typeof(bool), actual.GetType());
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ReturnStatement_ReturnsVariable() {
+            const int expected = 65;
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ int value = {expected}; return value; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerAddition_PositiveValues() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return 58 + 100; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(158, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerAddition_PositiveAndNegative_PositiveResult() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return -12 + 20; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(8, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerAddition_PositiveAndNegative_NegativeResult() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return -12 + 4; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(-8, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerAddition_NegativeValues() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return -12 + -4; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(-16, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerSubtraction_PositiveResult() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return 32 - 20; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(12, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerSubtraction_NegativeResult() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return 58 - 100; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(-42, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerSubtraction_DoubleNegative() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return 58 - -100; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(158, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerMultiplication() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return 6 * 7; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(42, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerDivision_WholeRemainder() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return 32 / 4; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(8, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerDivision_DecimalRemainder() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return 33 / 4; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(8, actual);
+        }
+
+        [Test]
+        public void Arithmetic_IntegerExponentiation() {
+            ParseTree program = Compiler.Compiler.Compile($"int main() {{ return 2 ^ 4; }}");
+            VirtualMachine vm = new VirtualMachine(program);
+
+            object actual = vm.Run();
+
+            Assert.AreEqual(typeof(int), actual.GetType());
+            Assert.AreEqual(16, actual);
         }
     }
 }
